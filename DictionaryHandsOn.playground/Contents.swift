a2f670034be4ae4f44db9221f1284a94711f925a
ShreyasTantri <shreyastantrik29@gmail.com>
2026-01-06 22:20:09 +0530

    Dictionaries updated

--- DictionaryHandsOn.playground/Contents.swift
+++ DictionaryHandsOn.playground/Contents.swift
@@ -1,1 +1,6 @@
-
+/*
+1.Word count: Write a function wordCounts(in:) that returns a dictionary of word → frequency for a string (split on spaces).
+2. Merge scoreboards: Given two [String: Int] scoreboards, merge them by summing scores for same players.
+3. Top N players: From a [String:Int] dictionary, produce an array of top 3 players by score.
+4. Normalize: Given ["a": 2, "b": 4] create a dictionary of floats normalized to sum to 1.0 using mapValues.
+*/
